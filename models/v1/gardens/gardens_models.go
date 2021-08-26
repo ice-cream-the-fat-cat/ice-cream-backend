d50086531e7903ba6130cfdd0aad4134cea9efe5
yukari-n-erb <45537257+yukari-n-erb@users.noreply.github.com>
2021-08-26 22:41:05 +0900

    fix gardens_models typo

--- models/v1/gardens/gardens_models.go
+++ models/v1/gardens/gardens_models.go
@@ -1,7 +1,7 @@
 package gardens_models
 
 type Gardens struct {
-	ID             string `bson:"_id,omitempty" json:"_id""`
+	ID             string `bson:"_id,omitempty" json:"_id"`
 	Name           string `bson:"name" json:"name"`
 	Description    string `bson:"description" json:"description"`
 	UserFireBaseId string `bson:"userFireBaseId" json:"userFireBaseId"`
