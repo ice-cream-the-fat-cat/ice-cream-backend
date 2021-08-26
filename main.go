16c78d1ab4a0e5fa4c79f544e043e99e85ff3d38
yukari-n-erb <45537257+yukari-n-erb@users.noreply.github.com>
2021-08-26 18:29:09 +0900

    change api path

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func createServer() {
 
 	router.HandleFunc("/icecream-test", routes.TestMongoDB).Methods("GET")
 
-	router.HandleFunc("/{version}/gardens/{id}", gardens_router.GardensIdGet).Methods("GET")
-	router.HandleFunc("/{version}/gardens/user/{userid}", gardens_router.GardensUserIdGet).Methods("GET")
-	router.HandleFunc("/{version}/gardens/", gardens_router.GardensPost).Methods("POST")
+	router.HandleFunc("/api/v1/gardens/{id}", gardens_router.GardensIdGet).Methods("GET")
+	router.HandleFunc("/api/v1/gardens/user/{userid}", gardens_router.GardensUserIdGet).Methods("GET")
+	router.HandleFunc("/api/v1/gardens/", gardens_router.GardensPost).Methods("POST")
 
 	router.HandleFunc("/api/v1/rules", rules_router.CreateRules).Methods("POST")
 
