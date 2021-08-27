021926028ffee777d3f4efd7a9cd0c6c4d33604d
kome12 <ko.take5481@gmail.com>
2021-08-27 22:04:00 +0900

    Add update rule by id

--- routes/v1/rules/rules_routes.go
+++ routes/v1/rules/rules_routes.go
@@ -3,11 +3,14 @@ package rules_router
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	rules_controllers "github.com/ice-cream-backend/controllers/v1/rules"
 	rules_models "github.com/ice-cream-backend/models/v1/rules"
 	"github.com/ice-cream-backend/utils"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func CreateRule(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +24,7 @@ func CreateRule(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "Error creating rules!")
 	} else {
-		newRule := rules_controllers.GetRulesById(res.InsertedID)
+		newRule := rules_controllers.GetRulesByRuleId(res.InsertedID)
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(newRule)
@@ -45,4 +48,44 @@ func CreateRules(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(newRules)
 	}
+}
+
+func EditRuleByRuleId(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: create edit rule by id")
+	vars := mux.Vars(r)
+	utils.EnableCors(&w)
+
+	var rule rules_models.Rules
+	_ = json.NewDecoder(r.Body).Decode(&rule)
+
+	paramsRuleId := vars["ruleId"]
+
+	oid, err := primitive.ObjectIDFromHex(paramsRuleId)
+
+	if err != nil {
+		log.Println("Error converting params ruleId to ObjectId:", err)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode("Invalid ruleId provided")
+	} else {
+		res, err := rules_controllers.UpdateRuleByRuleId(oid, rule)
+
+		if err != nil {
+			log.Println("Error updating rule:", err)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode("Error updating rule")
+		}
+
+		if res.MatchedCount != 0 {
+			updatedRule := rules_controllers.GetRulesByRuleId(oid)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(updatedRule)
+		} else {
+			log.Println("could not find matching rule ID:", oid)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode("Error updating rule: no matching oid")
+		}
+	}
+
+	
 }
