164a0c31ecbfe195cf47f62f9e18a07e2c44d2a3
kome12 <ko.take5481@gmail.com>
2021-09-10 23:05:23 +0900

    Update create completedTask response

--- models/v1/users/users_models.go
+++ models/v1/users/users_models.go
@@ -3,6 +3,7 @@ package users_models
 import (
 	"time"
 
+	completed_tasks_models "github.com/ice-cream-backend/models/v1/completed_tasks"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -13,4 +14,9 @@ type Users struct {
 	FlowerCollections []primitive.ObjectID `bson:"flowerCollections" json:"flowerCollections"`
 	CreatedDate       time.Time            `bson:"createdDate" json:"createdDate"`
 	LastUpdate        time.Time            `bson:"lastUpdate" json:"lastUpdate"`
+}
+
+type UserDataWithCompletedTask struct {
+	User	Users `json:"user"`
+	CompletedTask completed_tasks_models.CompletedTasks `json:"completedTask"`
 }
