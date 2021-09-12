829fa4617bd8b19e8049d4866c0e087317b2ff3d
kome12 <ko.take5481@gmail.com>
2021-09-12 17:06:14 +0900

    Add validation for PUT

--- models/v1/flowersStore/flowersStore_models.go
+++ models/v1/flowersStore/flowersStore_models.go
@@ -1,7 +1,13 @@
 package flowersStore_models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type FlowersStore struct {
 	FireBaseUserId string `bson:"fireBaseUserId" json:"fireBaseUserId"`
-	FlowerID       string `bson:"flowerId" json:"flowerId"`
+	FlowerID       primitive.ObjectID `bson:"flowerId" json:"flowerId"`
 	Price          int    `bson:"price" json:"price"`
+}
+
+func FlowerStoreValidation(flowerStore FlowersStore) bool {
+	return flowerStore.FireBaseUserId != "" && flowerStore.FlowerID != primitive.NilObjectID && flowerStore.Price != 0
 }
--- routes/v1/gardens/gardens_router.go
+++ routes/v1/gardens/gardens_router.go
@@ -23,7 +23,7 @@ func CreateGardens(w http.ResponseWriter, r *http.Request) {
 		var newGarden gardens_models.GardenForMongo
 		_ = json.NewDecoder(r.Body).Decode(&newGarden)
 
-		if gardens_models.GardenValidation(newGarden) {
+		if gardens_models.GardenMongoValidation(newGarden) {
 			res, err := gardens_controllers.CreateGardens(newGarden)
 
 			if err != nil {
@@ -39,7 +39,7 @@ func CreateGardens(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 		} else {
-			utils.SendErrorBack(w, fmt.Errorf("missiing required fields for creating garden: %+v", newGarden), "Missing required fields to create garden")
+			utils.SendErrorBack(w, fmt.Errorf("missing required fields for creating garden: %+v", newGarden), "Missing required fields to create garden")
 		}
 	}
 }
@@ -117,24 +117,28 @@ func UpdateGardenById(w http.ResponseWriter, r *http.Request) {
 
 		paramsGardenId := vars["gardenId"]
 
-		oid, err := primitive.ObjectIDFromHex(paramsGardenId)
-
-		if err != nil {
-			utils.SendErrorBack(w, err, "Error converting params gardenId to ObjectId")
-		} else {
-			res, err := gardens_controllers.UpdateGardenByGardenId(oid, garden)
+		if gardens_models.GardenValidation(garden) {
+			oid, err := primitive.ObjectIDFromHex(paramsGardenId)
 
 			if err != nil {
-				utils.SendErrorBack(w, err, "Error updating garden")
-			}
-
-			if res.MatchedCount != 0 {
-				updatedGarden, _ := gardens_controllers.GetGardensByGardenId(oid)
-
-				utils.SendResponseBack(w, updatedGarden, http.StatusOK)
+				utils.SendErrorBack(w, err, "Error converting params gardenId to ObjectId")
 			} else {
-				utils.SendErrorBack(w, fmt.Errorf("could not find matching garden ID: %s", oid), "Error updating rule: no matching oid")
+				res, err := gardens_controllers.UpdateGardenByGardenId(oid, garden)
+
+				if err != nil {
+					utils.SendErrorBack(w, err, "Error updating garden")
+				}
+
+				if res.MatchedCount != 0 {
+					updatedGarden, _ := gardens_controllers.GetGardensByGardenId(oid)
+
+					utils.SendResponseBack(w, updatedGarden, http.StatusOK)
+				} else {
+					utils.SendErrorBack(w, fmt.Errorf("could not find matching garden ID: %s", oid), "Error updating rule: no matching oid")
+				}
 			}
+		} else {
+			utils.SendErrorBack(w, fmt.Errorf("missing required fields update garden: %+v", garden), "Missing required fields to update garden")
 		}
 	}
 }
